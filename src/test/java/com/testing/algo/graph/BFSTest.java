93e6fc1e1d9ea96ae2097dfd157bf21a3ac6fa02
KetanGhungralekar <Ketan.Ghungralekar@iiitb.ac.in>
2025-11-24 16:45:37 +0530

    updated mutation limit

--- src/test/java/com/testing/algo/graph/BFSTest.java
+++ src/test/java/com/testing/algo/graph/BFSTest.java
@@ -131,4 +131,32 @@ public class BFSTest {
         BFS bfs = new BFS();
         assertTrue(bfs.hasPath(g, 1, 2));
     }
+
+    @Test
+    public void testHasPath_FailsWithoutVisitedMarking() {
+        Graph g = new Graph(false); // undirected graph
+
+        int[][] edges = {
+                { 31, 5 }, { 10, 46 }, { 19, 31 }, { 5, 1 }, { 31, 28 }, { 28, 29 }, { 8, 26 }, { 13, 23 }, { 16, 34 },
+                { 30, 1 },
+                { 16, 18 }, { 33, 46 }, { 27, 35 }, { 2, 25 }, { 49, 33 }, { 44, 19 }, { 22, 26 }, { 30, 13 },
+                { 27, 12 },
+                { 8, 16 }, { 42, 13 }, { 18, 3 }, { 21, 20 }, { 2, 17 }, { 5, 48 }, { 41, 37 }, { 39, 37 }, { 2, 11 },
+                { 20, 26 },
+                { 19, 43 }, { 45, 7 }, { 0, 21 }, { 44, 23 }, { 2, 39 }, { 27, 36 }, { 41, 48 }, { 17, 42 }, { 40, 32 },
+                { 2, 28 },
+                { 35, 38 }, { 3, 9 }, { 41, 30 }, { 5, 11 }, { 24, 22 }, { 39, 5 }, { 40, 31 }, { 18, 35 }, { 23, 39 },
+                { 20, 24 },
+                { 45, 12 }
+        };
+
+        for (int[] e : edges)
+            g.addEdge(e[0], e[1]);
+
+        BFS bfs = new BFS();
+
+        // In correct BFS, 0 and 49 are NOT connected
+        assertFalse(bfs.hasPath(g, 29, 46));
+    }
+
 }
